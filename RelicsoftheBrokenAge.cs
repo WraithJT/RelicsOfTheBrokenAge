9d1241c623cca8f089052a9ff835801383c8acc4
WraithJT <34054854+WraithJT@users.noreply.github.com>
2018-04-07 23:02:18 -0400

    Add project files.

---
+++ RelicsoftheBrokenAge.cs
@@ -0,0 +1,17 @@
+using Terraria.ModLoader;
+
+namespace RelicsoftheBrokenAge
+{
+	class RelicsoftheBrokenAge : Mod
+	{
+		public RelicsoftheBrokenAge()
+		{
+			Properties = new ModProperties()
+			{
+				Autoload = true,
+				AutoloadGores = true,
+				AutoloadSounds = true
+			};
+		}
+	}
+}
