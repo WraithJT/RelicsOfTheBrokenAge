31340477553e31d2d8b29bbdcfc81d562d8c00d3
JT <jeremylth@gmail.com>
2018-04-09 10:47:40 -0600

    Created Broken Soul

---
+++ Items/BrokenSoul.cs
@@ -0,0 +1,28 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RelicsoftheBrokenAge.Items
+{
+    public class BrokenSoul : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Broken Soul");
+            //Tooltip.SetDefault("This is a modded sword.");
+        }
+        public override void SetDefaults()
+        {
+            item.CloneDefaults(ItemID.SoulofNight);
+            item.color = Color.SlateGray;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DirtBlock, 10);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
