462f2ff0116feccee650bc1ad5ba30d0fcdba80e
JT <jeremylth@gmail.com>
2018-04-09 12:29:55 -0600

    Added drops for Broken Soul

---
+++ NPCs/RelicsoftheBrokenAgeGlobalNPC.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RelicsoftheBrokenAge.NPCs
+{
+    public class RelicsoftheBrokenAgeGlobalNPC : GlobalNPC
+    {
+        //public override bool InstancePerEntity
+        //{
+        //    get
+        //    {
+        //        return true;
+        //    }
+        //}
+
+
+        public override void NPCLoot(NPC npc)
+        {
+            if (Main.expertMode && !npc.friendly && npc.lifeMax > 10 && !npc.boss && Main.rand.Next(50) == 0)
+                Item.NewItem(npc.getRect(), mod.ItemType("BrokenSoul"));
+        }
+
+        //public override void DrawEffects(NPC npc, ref Color drawColor)
+        //{
+
+        //}
+
+        //public override void EditSpawnRate(Player player, ref int spawnRate, ref int maxSpawns)
+        //{
+
+        //}
+
+        //public override void SetupShop(int type, Chest shop, ref int nextSlot)
+        //{
+
+        //}
+    }
+}
