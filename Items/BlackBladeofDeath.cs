2a21485930a208d8ddd30664555f0e20b0814d5e
JT <jeremylth@gmail.com>
2018-04-10 08:35:37 -0600

    Creating final blade

---
+++ Items/BlackBladeofDeath.cs
@@ -0,0 +1,39 @@
+using Microsoft.Xna.Framework;
+using System;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RelicsoftheBrokenAge.Items
+{
+	public class StrangeSword : ModItem
+	{
+        public override string Texture
+        {
+            get { return "Terraria/Item_" + ItemID.EnchantedSword; }
+        }
+
+        public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Black Blade of Death");
+			Tooltip.SetDefault("This sword hungers for souls.");
+		}
+
+		public override void SetDefaults()
+		{
+            item.CloneDefaults(ItemID.EnchantedSword);
+            item.color = Color.DarkGray;
+            item.damage = 189;
+			item.rare = 8;
+		}
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "ReforgedSoul", 10);
+            recipe.AddIngredient(null, "StrangeSword", 1);
+            recipe.SetResult(this, 1);
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.AddRecipe();
+        }
+    }
+}
